b9586d5a9724b5a21a6671680e8445c2fee9a7cc
Даниил Белоусов <daniilbelousov@MacBook-Pro-Daniil.local>
2024-10-27 23:24:02 +0300

    a

--- internal/transport/http.go
+++ internal/transport/http.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/vndg-rdmt/paymentspod/internal/controller"
 )
 
 func NewHttp(ctr *controller.Fiber, host string) error {
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOriginsFunc: func(_ string) bool {
+			return true
+		},
+		AllowHeaders:     "",
+		AllowCredentials: true,
+	}))
 	app.Post("/api/payment", ctr.Payment)
 	app.Get("/api/status/:id", ctr.Status)
 
